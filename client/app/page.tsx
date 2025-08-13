cf9fc77e146fa6a27e27d8b420f01a80927ba841
vincentmaresca <vincentmaresca7@gmail.com>
2025-08-13 19:42:49 -0400

    Trying to figure out routing

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,13 +1,26 @@
+import Link from "next/link";
+
 export default async function Home(): Promise<unknown> {
 	/* const resource = 'https://route';
 	const options = 'idk?';
 	const res = await fetch(resource, options); */
 
 	const ngIfAlternative = true;
+	{/* <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+		{ngIfAlternative && <>Testing NgIfAlternatives Stuff</>}
+	</div> */}
 
 	return (
-		<div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-			{ngIfAlternative && <>Testing NgIfAlternatives Stuff</>}
-		</div>
+		<ul>
+			<li>
+				<Link href="/">Home</Link>
+			</li>
+			<li>
+				<Link href="/pages/test">Test</Link>
+			</li>
+			<li>
+				<Link href="/blog/hello-world">Blog Post</Link>
+			</li>
+		</ul>
 	);
 }
---
+++ client/app/pages/test.tsx
@@ -0,0 +1,11 @@
+import { Linden_Hill } from "next/font/google";
+import Link from "next/link";
+
+export default function Test() {
+	return (
+		<div>
+			Testing Page
+			<Link href="/">Back to Home Page</Link>
+		</div>
+	)
+}
